a17405e344b4754234fd9a8efbcb291bd54ae95a
taymarspencer <taymar.spen@gmail.com>
2026-08-22 05:05:13 -0400

    Add Rizo Core entry point

---
+++ src/rizo-core/index.js
@@ -0,0 +1,18 @@
+export { ContentRegistry } from "./registry.js";
+export { ContentQuery, createSelectors } from "./selectors.js";
+export { StateStore, createInitialPlayerState } from "./state-store.js";
+export { EventBus } from "./event-bus.js";
+export { GameHost, assertGameModule, createGameContext, GAME_CONTRACT_METHODS } from "./game-contract.js";
+export { MemoryPersistenceAdapter, LocalStoragePersistenceAdapter, createPersistenceController } from "./persistence.js";
+export { createRizoCore, mergeContentPacks, CORE_CATEGORIES } from "./core.js";
+
+import { createRizoCore } from "./core.js";
+import { RIZO_CONTENT, RIZO_REFERENCE_RULES } from "../content/index.js";
+
+export function createDefaultRizoCore(options = {}) {
+  return createRizoCore({
+    content: RIZO_CONTENT,
+    referenceRules: RIZO_REFERENCE_RULES,
+    ...options
+  });
+}
